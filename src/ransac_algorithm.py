c43db8009b60723842b8feb5f31be14b2a330dd6
Maximilian Salén <maximilian@live.se>
2024-09-10 21:31:45 +0200

    Moved  functions inside ransac_algorithm.py

--- src/ransac_algorithm.py
+++ src/ransac_algorithm.py
@@ -1,9 +1,9 @@
 import numpy as np
 import time
 import logging
+import scipy
 from tqdm import tqdm
-from .homography_to_RT import homography_to_RT
-from .essential_to_RT import essential_to_RT
+from .chirality_check import perform_chirality_check
 from .utils import *
 
 
@@ -327,3 +327,67 @@ def get_inlier_mask_H(
     mask = distances < threshold
 
     return mask
+
+
+def homography_to_RT(H, x1, x2):
+    def unitize(a, b):
+        denom = 1.0 / np.sqrt(a**2 + b**2)
+        return a * denom, b * denom
+
+    # Check the right sign for H
+    N = x1.shape[1]
+    if x1.shape[0] != 3:
+        x1 = np.vstack([x1, np.ones((1, N))])
+    if x2.shape[0] != 3:
+        x2 = np.vstack([x2, np.ones((1, N))])
+    positives = np.sum((np.sum(x2 * (np.dot(H, x1)), axis=0)) > 0)
+    if positives < N / 2:
+        H *= -1
+
+    U, S, Vt = np.linalg.svd(H)
+    s1 = S[0] / S[1]
+    s3 = S[2] / S[1]
+    zeta = s1 - s3
+    a1 = np.sqrt(1 - s3**2)
+    b1 = np.sqrt(s1**2 - 1)
+    a, b = unitize(a1, b1)
+    c, d = unitize(1 + s1 * s3, a1 * b1)
+    e, f = unitize(-b / s1, -a / s3)
+    v1 = Vt.T[:, 0]
+    v3 = Vt.T[:, 2]
+    n1 = b * v1 - a * v3
+    n2 = b * v1 + a * v3
+    R1 = U @ np.array([[c, 0, d], [0, 1, 0], [-d, 0, c]]) @ Vt
+    R2 = U @ np.array([[c, 0, -d], [0, 1, 0], [d, 0, c]]) @ Vt
+    t1 = e * v1 + f * v3
+    t2 = e * v1 - f * v3
+    if n1[2] < 0:
+        t1 = -t1
+        n1 = -n1
+    if n2[2] < 0:
+        t2 = -t2
+        n2 = -n2
+
+    # Move from Triggs' convention to H&Z notation
+    t1 = np.dot(R1, t1)
+    t2 = np.dot(R2, t2)
+
+    return R1, t1, R2, t2
+
+
+def essential_to_RT(E, K, x1, x2):
+    W = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
+    U, S, Vt = scipy.linalg.svd(E)
+    if np.linalg.det(U) * np.linalg.det(Vt) < 0:
+        Vt = -Vt
+    P1 = np.hstack((U @ W @ Vt, U[:, -1].reshape(-1, 1)))
+    P2 = np.hstack((U @ W @ Vt, -U[:, -1].reshape(-1, 1)))
+    P3 = np.hstack((U @ W.T @ Vt, U[:, -1].reshape(-1, 1)))
+    P4 = np.hstack((U @ W.T @ Vt, -U[:, -1].reshape(-1, 1)))
+
+    best_P, num_points_infront_of_cam = perform_chirality_check(
+        [P1, P2, P3, P4], K, x1, x2
+    )
+    best_R = best_P[:, :3]
+    best_T = best_P[:, 3]
+    return best_R, best_T, num_points_infront_of_cam
